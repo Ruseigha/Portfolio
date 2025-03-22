91e46bf7baea5f0317d464f9f42bed40f3b10c8b
Seigha Rufus <excellenceseigha@gmail.com>
2025-03-22 15:53:58 +0100

    modify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import visualizer from "vite-plugin-visualizer";
+
 export default defineConfig({
-  plugins: [react()],                                                    
-  base: "/Portfolio/"   
-})
+  base: "/Portfolio", // Change this if deploying under a subdirectory
+  plugins: [react(), visualizer()], // Add necessary plugins
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react-vendor";
+            if (id.includes("lodash")) return "lodash-vendor";
+            return "vendor"; // General vendor chunk
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase the chunk size warning limit
+  },
+});
+
+
